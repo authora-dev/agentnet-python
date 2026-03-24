eb2e8c6b676aabc313bffa062d476adf40f4774a
AgentNet <admin@authora.dev>
2026-03-24 17:26:03 +0200

    Release v0.1.5

--- agentnet/__init__.py
+++ agentnet/__init__.py
@@ -19,7 +19,7 @@ from .errors import (
 )
 from .resources.tasks import TasksResource, AsyncTasksResource
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = [
     "__version__",
     "AgentNetClient",
