3b8a3ecc6f8319a299a7b5a82c980dd83e9a1bbd
AgentNet <admin@authora.dev>
2026-03-24 17:18:16 +0200

    Bump version to 0.1.3

--- agentnet/__init__.py
+++ agentnet/__init__.py
@@ -19,7 +19,7 @@ from .errors import (
 )
 from .resources.tasks import TasksResource, AsyncTasksResource
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = [
     "__version__",
     "AgentNetClient",
